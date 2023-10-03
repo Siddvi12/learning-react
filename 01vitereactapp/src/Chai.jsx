e6243d9393c651746fd0d4f26e51a3130c96a0a3
Vikash Singh <138220005+Siddvi12@users.noreply.github.com>
2023-10-03 14:35:06 +0530

    before custom react

---
+++ 01vitereactapp/src/Chai.jsx
@@ -0,0 +1,8 @@
+function Chai (){
+
+    return(
+        <h3>chai with new component</h3>
+    )
+}
+
+export default Chai;
---
+++ custom-react/CustomReact.js
@@ -0,0 +1,11 @@
+const reactElements ={
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+
+    children: 'Click me to visit google'
+}
+
+const root = document.getElementById("root");
